858ce51d0669b865ec2245adbcdb1930de7db6c6
AguruC <aaclassiccom@gmail.com>
2025-04-26 16:29:54 +0200

    Fix?

--- main.lua
+++ main.lua
@@ -5,20 +5,20 @@ local buffTracker
 BA_SETTINGS = nil
 
 local function OnLoad()
-	BA_SETTINGS = require("better_archeage/settings")
+	BA_SETTINGS = require("better-archeage/settings")
 
 	if BA_SETTINGS.useRangeFinder then
-		rangeFinder = require("better_archeage/range_meter")
+		rangeFinder = require("better-archeage/range_meter")
 	end
 
   if BA_SETTINGS.useSpeedometer then
-  	speedoMeter = require("better_archeage/speedometer")	
+  	speedoMeter = require("better-archeage/speedometer")	
   end
-	require("better_archeage/better_frames")
+	require("better-archeage/better_frames")
 
 	-- require("better_archeage/abyss_bar")
   if BA_SETTINGS.useRangeFinder then
-		buffTracker = require("better_archeage/buff_tracker")
+		buffTracker = require("better-archeage/buff_tracker")
 	end
 end
 
@@ -47,7 +47,7 @@ return {
 	name = "Better ArcheAge",
 	desc = "A suite of improvements to the game",
 	author = "Aguru",
-	version = "1.1",
+	version = "1.2.1",
 	OnLoad = OnLoad,
 	OnUnload = OnUnload,
 	-- OnSettingToggle = OnSetting
